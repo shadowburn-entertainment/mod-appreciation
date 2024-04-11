d2e5eab120fba5455cfeb2e6c1ee868a3cdd18f9
Knindza <knindzagxg@gmail.com>
2024-04-11 19:04:24 +0200

    Update mod_appreciation.sql
    Updating for zhCN...

--- data/sql/db-world/base/mod_appreciation.sql
+++ data/sql/db-world/base/mod_appreciation.sql
@@ -13,8 +13,19 @@ DELETE FROM `creature_template` WHERE `entry`=@Entry;
 INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `IconName`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `unit_class`, `unit_flags`, `type`, `flags_extra`, `ScriptName`) VALUES
 (@Entry, 21697, 'Ambert Mendenhall', 'Appreciation Assistant', 'Speak', 60, 60, 35, 1, 1, 2, 7, 16777218, 'npc_appreciation');
 
-DELETE FROM `creature` WHERE `id1`=@Entry;
-INSERT INTO `creature` (`id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
-(@Entry, 0, -8877.62, 633.311, 96.0921, 6.06048),
-(@Entry, 1, 1736.17, -4332.46, 30.4142, 3.14391),
-(@Entry, 571, 5816.8, 622.221, 647.66, 1.73032);
+-- Adding translations for zhCN
+DELETE FROM `npc_text_locale` WHERE `ID`=6000000 AND `Locale`='zhCN';
+INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`, `Text0_1`, `Text1_0`, `Text1_1`, `Text2_0`, `Text2_1`, `Text3_0`, `Text3_1`, `Text4_0`, `Text4_1`, `Text5_0`, `Text5_1`, `Text6_0`, `Text6_1`, `Text7_0`, `Text7_1`) VALUES (6000000, 'zhCN', '你没有感谢证书！除非你给我带来一张，否则我无法帮助你。', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+DELETE FROM `npc_text_locale` WHERE `ID`=6000001 AND `Locale`='zhCN';
+INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`, `Text0_1`, `Text1_0`, `Text1_1`, `Text2_0`, `Text2_1`, `Text3_0`, `Text3_1`, `Text4_0`, `Text4_1`, `Text5_0`, `Text5_1`, `Text6_0`, `Text6_1`, `Text7_0`, `Text7_1`) VALUES (6000001, 'zhCN', '你想要哪个专业', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+DELETE FROM `npc_text_locale` WHERE `ID`=6000002 AND `Locale`='zhCN';
+INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`, `Text0_1`, `Text1_0`, `Text1_1`, `Text2_0`, `Text2_1`, `Text3_0`, `Text3_1`, `Text4_0`, `Text4_1`, `Text5_0`, `Text5_1`, `Text6_0`, `Text6_1`, `Text7_0`, `Text7_1`) VALUES (6000002, 'zhCN', '你确定你要做这件事吗？我必须销毁你目前的所有设备，所以确保你保存你不想丢失的东西!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+DELETE FROM `npc_text_locale` WHERE `ID`=6000003 AND `Locale`='zhCN';
+INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`, `Text0_1`, `Text1_0`, `Text1_1`, `Text2_0`, `Text2_1`, `Text3_0`, `Text3_1`, `Text4_0`, `Text4_1`, `Text5_0`, `Text5_1`, `Text6_0`, `Text6_1`, `Text7_0`, `Text7_1`) VALUES (6000003, 'zhCN', '你想要解锁哪个大陆？', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+DELETE FROM `creature_template_locale` WHERE `entry`=6000000 AND `locale`='zhCN';
+INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES (6000000, 'zhCN', '安伯特·门登霍尔', '感谢助手', 18019);
+
+DELETE FROM `item_template_locale` WHERE `ID`=20460 AND `locale`='zhCN';
+INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES (20460, 'zhCN', '感谢证书', '可以用来为你的角色提升等级！', 15050);
+ 
